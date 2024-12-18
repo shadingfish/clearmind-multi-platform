ba74be42b8548a0bb2d4c939d805bc46fcdb9da7
Dingyi Yu <yudingyi@DingyideMacBook-Pro.local>
2024-12-17 20:22:12 -0800

    Hide sensitive information: firebase, keystore

--- constants/firebaseConfig.ts
+++ constants/firebaseConfig.ts
@@ -1,18 +1,22 @@
 // constants/firebaseConfig.ts
 
-// Import necessary Firebase modules
+// Import necessary modules
 import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
+import * as dotenv from "dotenv";
 
-// Firebase configuration object
+// Load environment variables from the .env file
+dotenv.config();
+
+// Firebase configuration object using environment variables
 const firebaseConfig = {
-  apiKey: "AIzaSyAN0VTXfY575A6Hr-K225hEuCFAK1U9Tbs",
-  authDomain: "clearmind-442ad.firebaseapp.com",
-  databaseURL: "https://clearmind-442ad-default-rtdb.firebaseio.com",
-  projectId: "clearmind-442ad",
-  storageBucket: "clearmind-442ad.appspot.com",
-  messagingSenderId: "324251870640",
-  appId: "1:324251870640:android:28556d290c35015eff6275",
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
 };
 
 // Initialize Firebase app
