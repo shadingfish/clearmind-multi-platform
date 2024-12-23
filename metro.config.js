8fd44cedcb7a5832a77b7e99908b3ed920517101
tutingjun <50512161+tutingjun@users.noreply.github.com>
2024-12-23 14:50:30 +0800

    Add support for tamagui

--- metro.config.js
+++ metro.config.js
@@ -2,22 +2,36 @@
 
 const { getDefaultConfig } = require("@expo/metro-config");
 
-const config = getDefaultConfig(__dirname);
+const config = getDefaultConfig(__dirname, {
+  isCSSEnabled: true,
+});
 
 // Ensure the expo-asset plugin is included
 config.transformer = {
-    ...config.transformer,
-    assetPlugins: ["expo-asset/tools/hashAssetFiles"],
-  };
-  
-// Optional: If you're working with .svg files as React components
-config.resolver.assetExts = config.resolver.assetExts.filter((ext) => ext !== "svg");
-config.resolver.sourceExts.push("svg");
+  ...config.transformer,
+  assetPlugins: ["expo-asset/tools/hashAssetFiles"],
+};
 
-config.transformer.babelTransformerPath = require.resolve("react-native-svg-transformer");
+// Enable Tamagui and add nice web support with optimizing compiler + CSS extraction
+const { withTamagui } = require("@tamagui/metro-plugin");
+module.exports = withTamagui(config, {
+  components: ["tamagui"],
+  config: "./tamagui.config.ts",
+  outputCSS: "./tamagui-web.css",
+});
+
+// Optional: If you're working with .svg files as React components
+config.resolver.assetExts = config.resolver.assetExts.filter(
+  (ext) => ext !== "svg"
+);
+config.resolver.sourceExts.push("svg");
+config.resolver.sourceExts.push("mjs");
+
+config.transformer.babelTransformerPath = require.resolve(
+  "react-native-svg-transformer"
+);
 
 module.exports = config;
-
 
 // Clean version of metro.config.js
 
