5e04ed060079fbe88647fcf867bb1332762ed6ab
Dingyi Yu <yudingyi@DingyideMacBook-Pro.local>
2024-12-23 17:29:15 -0800

    finished 70% main page

--- app/(login)/index.tsx
+++ app/(login)/index.tsx
@@ -1,48 +1,120 @@
 // app/screens/MainScreen.tsx
 import React, { useState } from "react";
-import { View, Alert } from "react-native";
-import { CustomButton } from "../../components/CustomButton";
-import { InputField } from "../../components/InputField";
+import { Alert } from "react-native";
+import { Button, Input, Stack, Text, XStack, YStack } from "tamagui";
 import LogoImage from "../../components/LogoImage";
 import BackgroundImage from "../../components/BackgroundImage";
 import { useAuth } from "../../hooks/useAuth";
+import { useFonts } from "expo-font";
+import colors from "../../constants/colors";
 
 export default function MainScreen() {
   const { handleLogin } = useAuth();
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [fontsLoaded] = useFonts({
+    spacemono: require("../../assets/fonts/SpaceMono-Regular.ttf"),
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
 
   return (
-    <View style={{ flex: 1, backgroundColor: "#F1FED7" }}>
+    <YStack flex={1} >
       <BackgroundImage />
-      <View style={{ flex: 1, alignItems: "center", justifyContent: "center", paddingHorizontal: 16 }}>
-        <LogoImage />
-        <InputField
-          placeholder="Username"
-          value={username}
-          onChangeText={setUsername}
-        />
-        <InputField
-          placeholder="Password"
-          value={password}
-          onChangeText={setPassword}
-          secureTextEntry
-        />
-        <CustomButton
-          title="Sign In"
-          onPress={() => handleLogin(username, password)}
-        />
-        <CustomButton
-          title="Create Account"
-          onPress={() => Alert.alert("Register")}
-          variant="link"
-        />
-        <CustomButton
-          title="Forgot Password?"
-          onPress={() => Alert.alert("Forgot Password")}
-          variant="link"
-        />
-      </View>
-    </View>
+
+      <YStack
+        flex={1}
+        position="absolute"
+        alignItems="center"
+        justifyContent="center"
+        width="100%"
+        >
+          <Text 
+            marginTop="$8"
+            fontSize="$8" 
+            fontWeight="bold" 
+            color={colors.primary} 
+            textAlign="center"
+            width="100%"
+          >
+            Learn Acceptance and commitment therapy for free!
+          </Text>
+
+          {/* Logo */}
+          <LogoImage />
+
+          <Text 
+            marginTop="$4"
+            fontSize="$8" 
+            fontWeight="bold" 
+            color={colors.primary}
+            textAlign="center"
+            width="100%"
+          >
+            On-demand help for overcoming procrastination
+        </Text>
+      </YStack>
+
+      <YStack
+        flex={1}
+        alignItems="center"
+        justifyContent="flex-end"
+        paddingHorizontal="$4"
+        gap="$4"
+        position="absolute"
+        top={0}
+        left={0}
+        right={0}
+        bottom="$10"
+      >
+
+        <Stack width="100%" maxWidth={300} space="$3">
+          <Input
+            placeholder="Username"
+            value={username}
+            onChangeText={setUsername}
+          />
+          <Input
+            placeholder="Password"
+            value={password}
+            onChangeText={setPassword}
+            secureTextEntry
+          />
+        </Stack>
+
+        <Stack width="100%" maxWidth={300} space="$3">
+          <Button
+            size="$4"
+            onPress={() => handleLogin(username, password)}
+            theme="active"
+            color={colors.secondary}
+            fontWeight="bold"
+            backgroundColor={colors.primary}
+          >
+            Sign In
+          </Button>
+          <XStack justifyContent="space-between">
+            <Button
+              size="$3"
+              theme="alt2"
+              onPress={() => Alert.alert("Register")}
+              alignSelf="flex-start"
+            >
+              Create Account
+            </Button>
+            <Button
+              size="$3"
+              theme="alt2"
+              onPress={() => Alert.alert("Forgot Password")}
+              alignSelf="flex-end"
+            >
+              Forgot Password?
+            </Button>
+          </XStack>
+        </Stack>
+      </YStack>
+    </YStack>
   );
 }
---
+++ constants/colors.tsx
@@ -0,0 +1,10 @@
+const colors = {
+    primary: "#007F5F",
+    secondary: "#FFFFFF",
+    link: "transparent",
+    linkText: "#007F5F",
+    background: "#F1FED7",
+    text: "#000000",
+  };
+  
+  export default colors;
